b5a060bde9f0f0e0514e6f4e75ce2123d4dd0554
lctrayden25 <79618253+lctrayden25@users.noreply.github.com>
2023-10-13 02:32:08 +0800

    connect to mongodb

--- api/index.js
+++ api/index.js
@@ -1,6 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+	.connect(process.env.MONGODB)
+	.then(() => {
+		console.log("DB Connected.");
+	})
+	.catch((error) => {
+		console.log(error);
+	});
+
 const app = express();
 
 app.listen(3005, () => {
-    console.log("server is running on port 3005")
-})
+	console.log("server is running on port 3005");
+});
