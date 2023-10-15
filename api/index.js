14bf2fc9e49011a35d6620d770a8ad448f44cae3
lctrayden25 <79618253+lctrayden25@users.noreply.github.com>
2023-10-15 13:46:02 +0800

    create user update api

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 dotenv.config();
 
 import userRouter from "./routes/user.route.js";
@@ -18,6 +19,7 @@ mongoose
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
